bde669aefed0feacfa0d917a2fc5ead7f248d367
Melissa Longenberger <mlongenberger@codingdojo.com>
2022-11-30 15:55:28 -0500

    saving files pre class

---
+++ lectures/week1/session2.js
@@ -0,0 +1,33 @@
+// 1. Pull the current date and time
+
+
+// 2. Console.log the current date and time
+
+// 3. console.log the time only
+
+// 4. console.log the date only
+
+// 5a. What time zone is displaying?
+
+// 5b. Change to the different time zones (ET, CT, MT, PT)
+
+
+
+// Using the provided HTML file create functions to display things on the page (you can not edit the html document) and have a console.log for each
+
+
+// 6. Display date and time
+
+// 7a. Display time only
+
+// 7b. Include the current time zone in the display
+
+// 8. Display date only
+
+// 9. Display time only in each time zone listed
+
+
+// Bonus questions
+
+// 1. If there are differences in how the html displays and its corresponding console.log prints why?
+// 2. Are there other ways we can change what is displayed?
