c035b573d78abb2ef3216a165ede8f338f05d1dd
Instructor Melissa @Coding Dojo <78193873+dojo24@users.noreply.github.com>
2022-12-01 18:23:37 -0500

    Create session3.js

---
+++ lectures/week1/session3.js
@@ -0,0 +1,32 @@
+// 2 Classes Developer and Website - Need Constructor Function, Attributes, and Methods
+
+
+class Developer {
+  
+}
+
+
+
+
+
+
+class Website {
+  
+  
+}
+
+
+
+
+
+
+
+// Developer Instances
+
+
+
+// Website instances
+
+
+
+// Interacting with the created instances
